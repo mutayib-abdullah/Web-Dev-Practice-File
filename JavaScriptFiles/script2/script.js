b00eb9f761f91bb42a7e3f0e838737af5fcf27b8
mutayib-abdullah <muttayebkababi@gmail.com>
2026-06-09 18:15:28 +0530

    Variables

---
+++ JavaScriptFiles/script2/script.js
@@ -0,0 +1,27 @@
+// console.log("Hey its me here")
+// let a=6;
+// let b=5;
+// let c = "mutayib"
+// console.log(a+b)
+// console.log(typeof a, typeof b, typeof c)
+let x = "mutayib";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    "name": "mutayib",
+    "job code": 5600,
+    "is_handsome": true
+}
+console.log(o)
+
+o.salary = "100crores";
+console.log(o)
+o.salary = "500crores";
+console.log(o)
