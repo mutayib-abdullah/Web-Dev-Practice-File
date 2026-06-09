be7e4f3f20cf7a5d496a7b898a109da044af0ef7
mutayib-abdullah <muttayebkababi@gmail.com>
2026-06-09 17:00:00 +0530

    Fundamentals

---
+++ JavaScriptFiles/script1/script.js
@@ -0,0 +1,14 @@
+        alert("Hello world");
+console.log("code is running...")
+var a = prompt("Enter your number")
+var isTrue = confirm(" hey are you fine ")
+if(isTrue){
+    console.log("Im fine")
+}
+else{
+        console.log("Im not fine")
+
+}
+console.log(" Your number is " + a)
+document.title="hey im good"
+// document.body.style.backgroundColor="red"
