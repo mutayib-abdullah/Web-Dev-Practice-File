dbcabad48bbe22b6ca114c606f48416cb76ca71a
mutayib-abdullah <muttayebkababi@gmail.com>
2026-06-10 19:41:38 +0530

    Functions

---
+++ JavaScriptFiles/script5/script5.js
@@ -0,0 +1,34 @@
+
+// function nice(name) {
+//     console.log("hey " + name + " you are nice!");
+//     console.log("hey " + name + " you arent nice!");
+//     console.log("hey " + name + " you are bad!");
+//     console.log("hey " + name + " you are nice!");
+
+// }
+// nice("mutayib")
+// nice("abdullah")
+
+// function sum(a, b) {
+//     console.log(a + b);
+
+// }
+// sum(3, 4)
+// function sum(a, b, c=3) {
+// return a + b + c
+// }
+// result1 = sum(37, 4)
+// result2 = sum(335, 4)
+// result3 = sum(33, 4, 1)
+
+// console.log("The sum of this is:",result1);
+// console.log("The sum of this is:",result2);
+// console.log("The sum of this is:",result3);
+
+const func1 = (x) => {
+    console.log("I am an arrow functio ",x);
+    
+}
+func1(34)
+func1(4)
+func1(3)
