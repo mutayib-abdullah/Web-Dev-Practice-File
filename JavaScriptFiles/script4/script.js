08cb7949bb437f1d65f5f18d57e4112bd69be1bc
mutayib-abdullah <muttayebkababi@gmail.com>
2026-06-10 19:13:24 +0530

    Loops

---
+++ JavaScriptFiles/script4/script.js
@@ -0,0 +1,43 @@
+console.log("im a tutorial on loops")
+
+// let a = 1
+
+// for (let i = 0; i < 100; i++) {
+
+//     console.log(a + i);
+
+// }
+
+// let obj = {
+//     name: "Mutayib",
+//     role: "programmer",
+//     company: "codewithharry AI"
+// };
+
+// console.log(student.name);  // Mutayib
+// console.log(student.age);
+
+// for (const key in obj) {
+//     // if ( Object.hasOwnProperty.call(obj, key)) {
+//     const element = obj[key];
+//     console.log(element);
+
+// }
+//  for (const c of "Mutayib") {
+//     console.log(c);
+
+//  }
+
+// let i = 0;
+// while (i<6) {
+//     console.log(i);
+//     i++;
+// }
+
+let i = 10;
+
+do {
+    console.log(i);
+    i++;
+
+} while (i<6);
