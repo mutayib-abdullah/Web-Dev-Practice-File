3d80ef14b238455f041484b0c7cbe5a071e84f41
mutayib-abdullah <muttayebkababi@gmail.com>
2026-06-11 21:10:26 +0530

    Strings

---
+++ JavaScriptFiles/script6/script6.js
@@ -0,0 +1,14 @@
+console.log("Tutorial on Strings");
+let a = "Harry"
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
+// console.log(a[4]);
+
+// console.log(a.length);
+
+// let real_name = "harry"
+// let friend = " abdullah"
+// console.log(`His name is ${real_name} and his friends name is ${friend}`);
+
