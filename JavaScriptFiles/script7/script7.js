1a1331abe68c5072213ea5a4a752f8bd00bef8bd
mutayib-abdullah <muttayebkababi@gmail.com>
2026-06-17 19:21:21 +0530

    Arrays

---
+++ JavaScriptFiles/script7/script7.js
@@ -0,0 +1,30 @@
+let arr = [1, 2, 3, 4, 5]
+console.log(arr);
+// console.log(arr.length);
+// console.log(arr[0]);
+// console.log(arr, typeof arr);
+// console.log(arr.toString());
+// console.log(arr.join(" And "));
+// console.log(arr.pop());
+// console.log(arr.push("sd"));
+// console.log(arr);
+// console.log(arr.shift());
+// console.log(arr);
+
+// console.log(arr.unshift(44));
+// console.log(arr);
+// delete arr[1]
+// console.log(arr);
+
+// let a1 = [1,2,3,4]
+// let a2 = [11,22,33,44]
+// let a3 = [111,222,333,444]
+// console.log(a1,a2,a3);
+// a1.concat(a2,a3)
+// console.log(a1.concat(a2, a3));
+
+let ar = [5, 2, 8, 1];
+
+ar.sort();
+
+console.log(ar);
