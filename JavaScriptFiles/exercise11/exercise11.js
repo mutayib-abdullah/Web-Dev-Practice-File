4ef21b5679ef569f1ba23ef2e71a83a4f4f38c80
mutayib-abdullah <muttayebkababi@gmail.com>
2026-06-17 20:05:46 +0530

    exercise 11

--- JavaScriptFiles/exercise11/exercise11.js
+++ JavaScriptFiles/exercise11/exercise11.js
@@ -3,7 +3,7 @@ function factorial(number) {
     let arr = Array.from(Array(number + 1).keys())
     console.log(arr.slice(1,));
     let c = arr.slice(1,).reduce((a, b) => a * b)
-    console.log(c);
+    return c
 
 
 }
---
+++ JavaScriptFiles/exercise11/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+console.log(factorial(a));
